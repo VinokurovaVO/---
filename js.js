a9d4cd0847ceda20d3bdb3d2110666c79bd6cf46
Viktoria <44978123+VinokurovaVO@users.noreply.github.com>
2019-05-28 00:25:31 +0300

    Update js.js

--- js.js
+++ js.js
@@ -4,7 +4,6 @@ window.onload = function(){
 	}
 
 	var hod = 0;
-
 	document.getElementById('game').onclick = function(event){
 		console.log(event);
 		if (event.target.className == 'block'){
@@ -21,21 +20,28 @@ window.onload = function(){
 
 	function checkWinner(){
 		var allblock = document.getElementsByClassName('block');
-		if (allblock[0].innerHTML=='x' && allblock[1].innerHTML=='x' && allblock[2].innerHTML=='x') alert('Победа крестиков');
-		if (allblock[3].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[5].innerHTML=='x') alert('Победа крестиков');
-		if (allblock[6].innerHTML=='x' && allblock[7].innerHTML=='x' && allblock[8].innerHTML=='x') alert('Победа крестиков');
-		if (allblock[0].innerHTML=='x' && allblock[3].innerHTML=='x' && allblock[6].innerHTML=='x') alert('Победа крестиков');
-		if (allblock[1].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[7].innerHTML=='x') alert('Победа крестиков');
-		if (allblock[2].innerHTML=='x' && allblock[5].innerHTML=='x' && allblock[8].innerHTML=='x') alert('Победа крестиков');
-		if (allblock[0].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[8].innerHTML=='x') alert('Победа крестиков');
-		if (allblock[2].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[6].innerHTML=='x') alert('Победа крестиков');
+	if(allblock[0].innerHTML=='x' && allblock[1].innerHTML=='x' && allblock[2].innerHTML=='x') return alert('Победили крестики');
+    if(allblock[3].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[5].innerHTML=='x') return alert('Победили крестики');
+    if(allblock[6].innerHTML=='x' && allblock[7].innerHTML=='x' && allblock[8].innerHTML=='x') return alert('Победили крестики');
+    if(allblock[0].innerHTML=='x' && allblock[3].innerHTML=='x' && allblock[6].innerHTML=='x') return alert('Победили крестики');
+    if(allblock[1].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[7].innerHTML=='x') return alert('Победили крестики');
+    if(allblock[2].innerHTML=='x' && allblock[5].innerHTML=='x' && allblock[8].innerHTML=='x') return alert('Победили крестики');
+    if(allblock[0].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[8].innerHTML=='x') return alert('Победили крестики');
+    if(allblock[2].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[6].innerHTML=='x') return alert('Победили крестики');
 	
-		if (allblock[0].innerHTML=='0' && allblock[1].innerHTML=='0' && allblock[2].innerHTML=='0') alert('Победа ноликов');
-		if (allblock[3].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[5].innerHTML=='0') alert('Победа ноликов');
-		if (allblock[6].innerHTML=='0' && allblock[7].innerHTML=='0' && allblock[8].innerHTML=='0') alert('Победа ноликов');
-		if (allblock[0].innerHTML=='0' && allblock[3].innerHTML=='0' && allblock[6].innerHTML=='0') alert('Победа ноликов');
-		if (allblock[1].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[7].innerHTML=='0') alert('Победа ноликов');
-		if (allblock[2].innerHTML=='0' && allblock[5].innerHTML=='0' && allblock[8].innerHTML=='0') alert('Победа ноликов');
-		if (allblock[0].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[8].innerHTML=='0') alert('Победа ноликов');
-		if (allblock[2].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[6].innerHTML=='0') alert('Победа ноликов');
+	if(allblock[0].innerHTML=='О' && allblock[1].innerHTML=='О' && allblock[2].innerHTML=='О') return alert('Победили нолики');
+    if(allblock[3].innerHTML=='О' && allblock[4].innerHTML=='О' && allblock[5].innerHTML=='О') return alert('Победили нолики');
+    if(allblock[6].innerHTML=='О' && allblock[7].innerHTML=='О' && allblock[8].innerHTML=='О') return alert('Победили нолики');
+    if(allblock[0].innerHTML=='О' && allblock[3].innerHTML=='О' && allblock[6].innerHTML=='О') return alert('Победили нолики');
+    if(allblock[1].innerHTML=='О' && allblock[4].innerHTML=='О' && allblock[7].innerHTML=='О') return alert('Победили нолики');
+    if(allblock[2].innerHTML=='О' && allblock[5].innerHTML=='О' && allblock[8].innerHTML=='О') return alert('Победили нолики');
+    if(allblock[0].innerHTML=='О' && allblock[4].innerHTML=='О' && allblock[8].innerHTML=='О') return alert('Победили нолики');
+    if(allblock[2].innerHTML=='О' && allblock[4].innerHTML=='О' && allblock[6].innerHTML=='О') return alert('Победили нолики');
+	
+	if (hod > 7) alert('Ничья!');
 	}
+	$('#button_wrapper').click(function(){
+        location.reload();
+    });
+
+}
